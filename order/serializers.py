d33fcdf33f2cf81ccfbe2b0799f41d93cf0a3101
Dev Raj Singh <rehansingh.4522@gmail.com>
2023-02-13 00:50:23 +0530

    CartView

--- order/serializers.py
+++ order/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
-
+from product.models import Product
 from .models import Order, Txn
+from django.core.exceptions import ObjectDoesNotExist
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        depth=1
-        fields = '__all__'
-
+        fields = ("id", "user", "product", "status", "is_active")
+   
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        try:
+            print(data)
+            data['product'] = {"id": instance.product.id, "name": instance.product.name, "images": instance.product.images, "price": instance.product.price}
+        except ObjectDoesNotExist:
+            pass
+        return data
+        
 class TxnSerializer(serializers.ModelSerializer):
     class Meta:
         model = Txn
