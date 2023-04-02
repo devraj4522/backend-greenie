91eb536687072a9f224b2c4b7df75eb422e257de
Dev Raj Singh <rehansing.4522@gmail.com>
2023-04-02 21:28:15 +0530

    CSRF_TRUSTED_ORIGINS

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,12 +13,13 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "backend-greenie-production-efac.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['*']
+
 
 RAILWAYS_EXTERNAL_HOSTNAME = os.environ.get('RAILWAYS_EXTERNAL_HOSTNAME')
 if RAILWAYS_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RAILWAYS_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
