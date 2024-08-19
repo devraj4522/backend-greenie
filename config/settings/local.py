5e75f335ddcab7f0dd7e7719164f1f442ae3e5f0
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2024-08-19 23:26:33 +0530

    Update local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "backend-greenie-production-efac.up.railway.app", "backend-greenie.onrender.com"]
-
+ALLOWED_HOSTS += env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
 RAILWAYS_EXTERNAL_HOSTNAME = os.environ.get('RAILWAYS_EXTERNAL_HOSTNAME')
 if RAILWAYS_EXTERNAL_HOSTNAME:
